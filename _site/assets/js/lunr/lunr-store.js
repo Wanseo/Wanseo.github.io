6462c8f4c3f612dbc9816f1953a3e63d96dd24e2
조완서 <jowanseo@jowanseoui-MacBook-Pro.local>
2021-05-19 03:47:53 -0700

    setting

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "About",
+        "excerpt":"   꿈을 이루기 위해 노력하면, 노력이 모여 곧 재능이 된다고 믿는다. 재능이 되면 내가 필요한 곳에서 쓰임받을 수 있을거라고 믿는다.    안녕하세요, 저는 꿈을 현실로 만드는 개발자 조완서 입니다.  ","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/first-post/",
+        "teaser": null
+      }]
